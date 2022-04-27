4dfb42417d6539d32ddde0b45720a73b46fc689d
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-04-27 00:40:33 -0300

    criando_a_barra_de_navegação_do_site

--- Eventos/resources/views/layouts/main.blade.php
+++ Eventos/resources/views/layouts/main.blade.php
@@ -8,7 +8,7 @@
 
         <!-- Fonte do Google -->
         <link href="https://fonts.googleapis.com/css2?family=Roboto" rel="stylesheet">
-        
+
         <!-- CSS Bootstrap -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
@@ -18,6 +18,33 @@
 
     </head>
     <body>
+        <header>
+            <nav class="navbar navbar-expand-lg navbar-light">
+                <div class="collapse navbar-collapse" id="navbar">
+                    <a href="/" class="navbar-brand">
+                        <img src="/img/show.jpg" alt="logo">
+                    </a>
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a href="/" class="nav-link">Eventos</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a href="/" class="nav-link">Criar Eventos</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a href="/" class="nav-link">Entrar</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a href="/" class="nav-link">Cadastrar</a>
+                            </li>
+                        </ul>
+
+                </div>
+            </nav>
+        </header>
         @yield('content')
         <footer>
             <p>Luis Henrique S F &copy; 2022</p>
